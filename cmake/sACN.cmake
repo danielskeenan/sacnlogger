659dee256618b3745371ff048e8492713aa25bb6
Dan Keenan <dk@dankeenan.org>
2025-03-02 13:21:00 -0500

    Packaging.

--- cmake/sACN.cmake
+++ cmake/sACN.cmake
@@ -2,5 +2,6 @@ include(FetchContent)
 FetchContent_Declare(sACN
         GIT_REPOSITORY "https://github.com/ETCLabs/sACN.git"
         GIT_TAG "v4.0.0.3"
+        EXCLUDE_FROM_ALL
 )
 FetchContent_MakeAvailable(sACN)
